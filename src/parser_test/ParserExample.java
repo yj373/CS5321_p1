e435e7cf46cf09f053b7566050f2c4f6a1143a79
Yixuan Jiang <yixuanjiang@YixuandeMacBook-Air.local>
2018-09-26 21:54:45 -0400

    add p1

---
+++ src/parser_test/ParserExample.java
@@ -0,0 +1,31 @@
+package parser_test;
+import java.io.FileReader;
+import net.sf.jsqlparser.parser.CCJSqlParser;
+import net.sf.jsqlparser.statement.Statement;
+import net.sf.jsqlparser.statement.select.Select;
+import net.sf.jsqlparser.statement.select.PlainSelect;
+
+public class ParserExample {
+	
+	private static final String queriesFile = "queries.sql";
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			CCJSqlParser parser = new CCJSqlParser(new FileReader(queriesFile));
+			Statement statement;
+			while ((statement = parser.Statement()) != null) {
+				System.out.println("Read statement: " + statement);
+				Select select = (Select) statement;
+				PlainSelect ps = (PlainSelect)select.getSelectBody();
+				System.out.println(ps.getFromItem().toString());
+				System.out.println("Select body is " + select.getSelectBody());
+			}
+		} catch (Exception e) {
+			System.err.println("Exception occurred during parsing");
+			e.printStackTrace();
+		}
+
+	}
+
+}
