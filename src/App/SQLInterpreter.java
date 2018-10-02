50c6f35eb5d0830c1f0fa520c3f0a209ba47f3a8
Yixuan Jiang <yixuanjiang@dhcp-hol-1373.eduroam.cornell.edu>
2018-10-01 23:01:11 -0400

    debug parser

--- src/App/SQLInterpreter.java
+++ src/App/SQLInterpreter.java
@@ -1,13 +1,11 @@
 package App;
 
-import java.beans.Statement;
 import java.io.FileReader;
+
 import data.Dynamic_properties;
 import net.sf.jsqlparser.parser.CCJSqlParser;
 import net.sf.jsqlparser.statement.select.Select;
 import operators.Operator;
-import visitors.BasicVisitor;
-import net.sf.jsqlparser.statement.select.PlainSelect;
 
 
 public class SQLInterpreter {
@@ -21,35 +19,12 @@ public class SQLInterpreter {
 		String queriesFile = Dynamic_properties.queryPath;
 		try {
 			CCJSqlParser parser = new CCJSqlParser(new FileReader(queriesFile));
-			Statement statement;
+			net.sf.jsqlparser.statement.Statement statement;
 			int index = 1;
-			while ((statement = (Statement) parser.Statement()) != null) {
+			while ((statement = parser.Statement()) != null) {
 				System.out.println("Read statement: " + statement);
 				Select select = (Select) statement;
 			}
-				
-//
-//		String queriesFile = Dynamic_properties.queryPath;
-//		try {
-//			Operator root = null;
-//			BasicVisitor visitor = new BasicVisitor();
-//			
-//		
-//			
-//			
-//			
-//			
-//			
-//			CCJSqlParser parser = new CCJSqlParser(new FileReader(queriesFile));
-//			Statement statement = (Statement) parser.Statement();
-//			int index = 1;
-//
-//			while (statement != null) {
-//				Select select = (Select)statement;
-//				root = visitor.getQueryPlan(select);
-//				writeToFile (index, root);
-//				index++;
-//			}
 
 
 		} catch (Exception e){
