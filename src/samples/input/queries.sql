358598f6c43d2a8911df4e401d770ad86a423709
yanxiaoxing <xy363@cornell.edu>
2018-10-02 17:13:59 -0400

    update

--- src/samples/input/queries.sql
+++ src/samples/input/queries.sql
@@ -1,5 +1,4 @@
-SELECT * FROM Sailors S, Reserves R WHERE S.A = R.G;
-SELECT * FROM Sailors S, Reserves R WHERE R.G = S.A;
+SELECT S.* FROM Sailors;
 SELECT * FROM Sailors, Reserves WHERE Sailors.A = Reserves.G;
 SELECT * FROM Sailors S1, Sailors S2 WHERE S1.A < S2.A AND S1.A<3; 
 SELECT * FROM Sailors S1, Sailors S2 WHERE S1.A < S2.A AND s1.A<3; 
@@ -10,4 +9,6 @@ SELECT Sailors.A FROM Sailors;
 SELECT S.A FROM Sailors S;
 SELECT * FROM Sailors S WHERE S.A < 3;
 SELECT * FROM Sailors, Reserves WHERE Sailors.A = Reserves.G;
+
+
 
